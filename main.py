5b69e5152b6fc9f38172a1a50b01541c80ae097e
rajakishorekavi <rajakishorekavi@gmail.com>
2022-02-01 17:21:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,4 +15,4 @@ def hello():
     return "Successfully installed all the Libraries and is running. Upload the files now"
 
 if __name__ == '__main__':
-    app.run("0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
